adbe9b93005f8b518ab33b18e9f663ecb47b57fc
KraYFS <grigorievdaniil13@gmail.com>
2026-03-06 23:18:03 +0200

    changed styles name to BEM

--- UI/InfoPoint/index.tsx
+++ UI/InfoPoint/index.tsx
@@ -1,11 +1,18 @@
 import './styles.scss'
 
-const InfoPoint = () => {
+type InfoPointProps = {
+  text?: string
+}
+
+const InfoPoint = ({ text }: InfoPointProps) => {
   return (
-    <svg className='line' viewBox='0 0 300 120'>
-      <polyline className='line-path' points='260,80 200,20 0,20' />
-      <circle cx='260' cy='80' r='4' className='line-dot' />
-    </svg>
+    <div >
+      <svg className='line' viewBox='0 0 300 120'>
+        <polyline className='line__path' points='260,80 200,20 0,20' />
+        <circle cx='260' cy='80' r='4' className='line__dot' />
+      </svg>
+      {text}
+    </div>
   )
 }
 
